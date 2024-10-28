fba5fdc788f2f4c5158a4724b509c840fce20eae
Bogadhikavitha <Bogadhikavitha@gmail.com>
2024-10-28 12:33:29 +0530

    Create month.c

---
+++ month.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    char month;
+ printf("enter the number");
+    scanf("%d",&num);
+    switch(month)
+    {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12: printf("%d is a num",month);
+        break;
+        default:printf("invalid");
+    }
+}
+        
