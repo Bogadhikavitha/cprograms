9486c72fe0f7974675a82f5017dc67b3bb1b88c6
Bogadhikavitha <Bogadhikavitha@gmail.com>
2024-12-30 11:38:29 +0530

    Create pattern3.c

---
+++ pattern3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main()
+{ int i,j;
+char n='A';
+    for(i=0; i<5; ++i)
+    {  
+        for(j=0; j<i; ++j)
+       {
+      printf("%c",n++);
+    }
+    printf("\n");
+}
+}
