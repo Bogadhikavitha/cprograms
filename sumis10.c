c0ce3805739542ecc7cb17c3841dcd6a97fc0185
Bogadhikavitha <Bogadhikavitha@gmail.com>
2025-01-13 12:02:32 +0530

    Update

---
+++ sumis10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+   
+   int a,b,sum=0;
+    
+    printf("Enter the elements");
+      scanf("%d", &a,&b);
+ {
+     sum=a+b;
+          }
+   printf("sum is closest to 10",sum);
+}
