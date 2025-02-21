774cc0fc628c715da8fc3bd2b881901e0d2e3836
Bogadhikavitha <Bogadhikavitha@gmail.com>
2025-02-21 20:52:13 +0530

    Update

---
+++ min array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() 
+{
+ int a[10],n,i,max,min;
+ printf("enter size");
+ scanf("%d",&n);
+ printf("enter the elements ");
+ for(i=0; i<n; i++)
+ {
+     scanf("%d",&a[i]);
+     } max=a[0];
+ min=a[0];
+ for (i=0; i<n;i++)
+ {
+    if(a[i]>max)
+    {
+        max=a[i];
+    }
+ } if(a[i]<min)
+ {
+     min=a[i];
+ }
+    printf(" %d",a[i]);
+}
